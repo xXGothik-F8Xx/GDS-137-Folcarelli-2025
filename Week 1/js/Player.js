9cb63c43e3e8fda5311915d25b7406cbaab4686d
xXGothik-F8Xx <ajfolcarelli@email.neit.edu>
2025-03-27 10:52:25 -0400

    Week-1
    Made a ball animation for the program

---
+++ Week 1/js/Player.js
@@ -0,0 +1,39 @@
+// JavaScript Document
+function Player()
+{
+	//player's location
+	this.x = canvas.width/2;
+	this.y = canvas.height/2;
+	
+	//player's dimensions
+	this.width = 100;
+	this.height = 100;
+	
+	//player's velocity or speed on each axis
+	this.vx = 0;
+	this.vy = 0;
+	
+	//player's color
+	this.color = "008000";
+	
+	//This draws the player to the screen
+	this.draw = function()
+	{
+		context.save();
+			context.fillStyle = this.color;
+			context.translate(this.x, this.y);
+			context.beginPath();
+			context.arc(0,0,this.width/2,0,360*Math.PI/180,true)
+			context.closePath();
+			context.fill();
+		context.restore();
+		
+	}	
+	
+	//This changes the player's position
+	this.move = function()
+	{
+		this.x += this.vx;
+		this.y += this.vy;
+	}
+}
---
+++ Week 1/js/week1_boundary_detection_bounce.js
@@ -0,0 +1,55 @@
+// JavaScript Document
+
+var canvas;
+var context;
+var timer;
+var interval = 1000/60;
+var player;
+
+	canvas = document.getElementById("canvas");
+	context = canvas.getContext("2d");	
+	player = new Player();
+	
+	//------Declare the Player's speed on the x and y axis------
+	player.vx = 5;
+	player.vy = 5;
+	//----------------------------------------------------
+	
+	timer = setInterval(animate, interval);
+
+
+function animate()
+{
+	context.clearRect(0,0,canvas.width, canvas.height);	
+	
+	//----Movement Using the Player's move() function----
+	player.move();
+	//---------------------------------------------------
+	
+	//--------------Bounce of Right----------------------
+	if(player.x > canvas.width - player.width/2)
+	{
+		player.x = canvas.width - player.width/2;
+		player.vx = -player.vx;	
+	};
+	
+	if(player.x < 0 + player.width/2)
+		{
+			player.x = 0 + player.width/2;
+			player.vx = -player.vx;	
+		};
+		if(player.y < 0 + player.height/2)
+			{
+				player.y = 0 + player.height/2;
+				player.vy = -player.vy;	
+			};
+			if(player.y > canvas.height - player.height/2)
+				{
+					player.y = canvas.height - player.height/2;
+					player.vy = -player.vy;	
+				};
+				
+	//---------------------------------------------------
+	
+	player.draw();
+}
